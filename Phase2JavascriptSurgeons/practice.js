c4d76b738d1ed2ff9cf3b5632d8e54d51743cd49
Gaurav Yadav Ubuntu <gyadav187@gmail.com>
2025-08-27 17:59:13 +0530

    Array problems done

--- Phase2JavascriptSurgeons/practice.js
+++ Phase2JavascriptSurgeons/practice.js
@@ -20,12 +20,48 @@ const obj = {
 	secondKey: 123,
 	anotherKey: true,
 };
-let gaurav;
-console.log(typeof null); // object
-console.log(typeof "Hello"); // string
-console.log(typeof 123); // number
-console.log(typeof true); // boolean
-console.log(typeof gaurav); // undefined
-console.log(typeof fruits); // object
-console.log(typeof obj); // object
-console.log(typeof checkIfFailOrPass);
+// let gaurav;
+// console.log(typeof null); // object
+// console.log(typeof "Hello"); // string
+// console.log(typeof 123); // number
+// console.log(typeof true); // boolean
+// console.log(typeof gaurav); // undefined
+// console.log(typeof fruits); // object
+// console.log(typeof obj); // object
+// console.log(typeof checkIfFailOrPass);
+
+const teas = ["Green tea", "Black Tea", "Oolong Tea", "White tea", "Herbal Tea"];
+teas.push("Chamomile Tea");
+console.log(teas);
+
+const indexofOolong = teas.indexOf("Oolong Tea");
+console.log(indexofOolong);
+
+teas.splice(indexofOolong,1);
+console.log(teas);
+
+
+
+console.log(teas.reduce((acc, curr)=>curr!="Herbal Tea" ?  acc++: acc, 0));
+// console.log(teas.reduce((acc, curr)=>{
+// 	if(curr!="Herbal Tea"){
+// 		acc++;
+// 	}
+// 	return acc;
+// },0));
+
+console.log(teas.map((curr) => curr.toUpperCase()));
+
+const initial = {
+	maxCount: 0,
+	item: ""
+}
+
+console.log(teas.reduce((acc, curr)=>{
+	let lengtho = curr.length;
+	if(lengtho>acc.maxCount){
+		acc.maxCount = lengtho;
+		acc.item = curr
+	}
+	return acc;
+},initial));
