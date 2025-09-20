13e718d0d68a2c61ed653c7f4a77206d30c16628
Gaurav Yadav <gyadav187@gmail.com>
2025-09-20 10:49:41 +0530

    Finishing up

--- Phase2JavascriptSurgeons/practice.js
+++ Phase2JavascriptSurgeons/practice.js
@@ -46,36 +46,82 @@
 
 // object2.hello();
 
-function Hero(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+// -----------------------------------------------------------------------
+
+// function Hero(firstName, lastName) {
+// 	this.firstName = firstName;
+// 	this.lastName = lastName;
+// }
+
+// Hero.prototype.shoutName = function () {
+// 	console.log(`My name is Mr. ${this.firstName} ${this.lastName}`);
+// };
+
+// Hero.prototype.alone = function () {
+// 	console.log("I am alone in this world");
+// };
+
+// const anthony = new Hero("Anthony", "Gonsalvez");
+
+// anthony.shoutName();
+// anthony.alone();
+
+// ------------------------------------------------------------------------
+
+// const carIn1940 = {
+// 	drive() {
+// 		console.log("The driver is now driving the car");
+// 	},
+// };
+
+// const carIn1970 = Object.create(carIn1940);
+// carIn1970.playMusic = function () {
+// 	console.log("Now you can also listen to music while driving the car");
+// };
+
+// const carIn2000 = Object.create(carIn1970);
+// carIn2000.watchMovieInBackSeat = function () {
+// 	console.log("Now you can watch a movie while the driver drives the car");
+// };
+
+// carIn2000.drive(); // The driver is now driving the car
+// carIn2000.playMusic(); // Now you can also listen to music while driving the car
+// carIn2000.watchMovieInBackSeat(); // Now you can watch a movie while the driver drives the car
+
+// carIn1970.drive(); // The driver is now driving the car
+// carIn1970.playMusic(); // Now you can also listen to music while driving the car
+
+// carIn1940.drive(); //The driver is now driving the car
+
+// // carIn2000.selfDrive();
+
+// const carIn2025 = Object.create(carIn2000);
+
+// carIn2025.drive = function () {
+// 	console.log("No need of a driver. Autobots to the rescue. Sit back, relax!");
+// };
+
+// carIn2025.drive();
+// -------------------------------------------------------------------------
+
+// function Pizza(description) {
+// 	this.description = description;
+// }
+// const aSimplePizza = new Pizza("Simple Pizza");
+
+class Car {
+	constructor(name, year) {
+		this.name = name;
+		this.year = year;
+	}
+
+	drive() {
+		console.log("The driver is now driving the car");
+	}
 }
 
-Hero.prototype.shoutName = function () {
-  console.log(`My name is Mr. ${this.firstName} ${this.lastName}`);
-};
+const carIn1940 = new Car("Just Car", 1940);
 
-Hero.prototype.alone = function () {
-  console.log("I am alone in this world");
-};
+carIn1940.drive();
 
-const anthony = new Hero("Anthony", "Gonsalvez");
-
-anthony.shoutName();
-anthony.alone();
-
-const carIn1940 = {
-  drive() {
-    console.log("The driver is now driving the car");
-  },
-};
-
-const carIn1970 = Object.create(carIn1940);
-carIn1970.playMusic = function () {
-  console.log("Now you can also listen to music while driving the car");
-};
-
-const carIn2000 = Object.create(carIn1970);
-carIn2000.watchMovieInBackSeat = function () {
-  console.log("Now you can watch a movie while the driver drives the car");
-};
+const carIn1970 = new Car();
